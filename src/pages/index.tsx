0e9c4aefee4c5cd00f0886179725d779d91f15dc
dotarsojat69 <tdeath457@gmail.com>
2024-03-11 14:57:57 +0700

    feat: update sedikit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import { getPokemonImage, getPokemons } from "@/utils/apis/pokemons/api"
 import { useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom"
+
 interface pokemonData {
   name: string,
   image: string
@@ -62,9 +63,11 @@ const App = () => {
             </Link>
           ))}
         </div>
-        <div className="w-full text-slate-900 flex justify-between text-2xl py-12 sm:py-5">
-          <button className="py-2 px-5 border bg-white border-slate-400 disabled:cursor-not-allowed rounded-lg" disabled={!prevPage} onClick={() => fetchPokemons(prevPage)}>Prev</button>
-          <button className="py-2 px-5 border bg-white border-slate-400 disabled:cursor-not-allowed rounded-lg" disabled={!nextPage} onClick={() => fetchPokemons(nextPage)}>Next</button>
+        <div className="col-span-2 flex justify-between">
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 192 512" className="h-10 w-10 disabled:cursor-not-allowed text-black dark:text-white" aria-disabled={!prevPage} onClick={() => fetchPokemons(prevPage)} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+            <path d="M192 127.338v257.324c0 17.818-21.543 26.741-34.142 14.142L29.196 270.142c-7.81-7.81-7.81-20.474 0-28.284l128.662-128.662c12.599-12.6 34.142-3.676 34.142 14.142z"></path></svg>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 192 512" className="h-10 w-10 disabled:cursor-not-allowed text-black dark:text-white" aria-disabled={!nextPage} onClick={() => fetchPokemons(nextPage)} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path></svg>
         </div>
       </div>
     </Layout>
